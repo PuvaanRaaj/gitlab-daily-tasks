ad6f736129ffe4edd99046330dee38c3f7eb4787
PuvaanRaaj <puvaan.shankar@fiuu.com>
2025-07-21 00:23:41 +0800

    add model

---
+++ models/user.py
@@ -0,0 +1,20 @@
+import enum
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class UserStatus(enum.Enum):
+    DELETED = 0
+    ACTIVE = 1
+    BLOCKED = 2
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    display_name = db.Column(db.String(100))
+    team_lead = db.Column(db.String(100))
+    gitlab_username = db.Column(db.String(100))
+    token = db.Column(db.String(200))
+    job_title = db.Column(db.String(100))
+    status = db.Column(db.Enum(UserStatus), default=UserStatus.ACTIVE)
